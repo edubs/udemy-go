3444adf1d50ef049b62556aed82ad4172778717b
edubs <11930020+edubs@users.noreply.github.com>
2022-01-12 13:31:09 -0500

    add test for read/write file actions

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,5 +18,18 @@ func TestNewDeck(t *testing.T) {
 
 	if d[len(d)-1] != "King of Spades" {
 		t.Errorf("Expected last card to be King of Spades but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndReadFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := readFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52 but got %v", len(d))
 	}
 }
