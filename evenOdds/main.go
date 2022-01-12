46cd98b80603c01fff4f7fb9cb465803d9ce1ca1
edubs <11930020+edubs@users.noreply.github.com>
2022-01-12 13:52:12 -0500

    easier loop

--- evenOdds/main.go
+++ evenOdds/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	mints := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i, num := range mints {
-		if mints[num]%2 != 0 {
-			fmt.Printf("%v is odd\n", mints[i])
+	for _, num := range mints {
+		if num%2 != 0 {
+			fmt.Printf("%v is odd\n", num)
 		} else {
-			fmt.Printf("%v is even\n", mints[i])
+			fmt.Printf("%v is even\n", num)
 		}
 	}
 
