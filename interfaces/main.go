cfb74f59a43353dd6ad5c7a956d7568db76f2f4d
edubs <11930020+edubs@users.noreply.github.com>
2022-01-14 09:44:06 -0500

    interfaces 101

---
+++ interfaces/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+type englishBot struct {
+}
+
+type spanishBot struct {
+}
+
+func main() {
+	/*
+		this is how you declare a new a new value of type struct
+		when the struct has no fields associated with it
+	*/
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+/*
+	if you are a type that has a function called getGreeting()
+	that returns a string, then you are also of type 'bot' via
+	the intereface. this allows both eb and sb to call printGreeting
+	even though they are not directly of type 'bot'
+*/
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola amigo!"
+}
